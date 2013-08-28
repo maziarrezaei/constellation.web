f6d602cc65f48f807b3a504357ad2ebcf960cea5
Richard Cabral <richard@viridianspark.com>
2013-08-28 10:04:03 -0400

    Ported from ViridianSpark library. Passes initial Stylecop tests.

---
+++ Pagination/IPaginationControl.cs
@@ -0,0 +1,21 @@
+﻿namespace Spark.Web.Pagination
+{
+	/// <summary>
+	/// Contract for use with Pagination Controls.
+	/// </summary>
+	public interface IPaginationControl
+	{
+		/// <summary>
+		/// Gets or sets the paginator that handles pagination logic.
+		/// </summary>
+		Paginator Paginator { get; set; }
+
+		/// <summary>
+		/// Gets the current page, resolved by the pagination control.
+		/// </summary>
+		int CurrentPage
+		{
+			get;
+		}
+	}
+}
